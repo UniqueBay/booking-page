abf245802175a99cd1e9660fcfb544b6c2df909a
Alabi Abayomi <abayomi15@gmail.com>
2021-06-10 11:59:35 +0100

    added alert function and fixed typos/styling

---
+++ asset/js/main.js
@@ -0,0 +1,7 @@
+function fillForm() {
+    var a = document.forms["fill"]["name"].value;
+    var b = document.forms["fill"]["mail"].value;
+    var c = document.forms["fill"]["phone"].value;
+
+    alert("Your name is "+a+". Your e-mail is "+b+". Your phone number is "+c);
+}
